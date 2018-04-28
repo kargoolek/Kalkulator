81ef3175eaa525fc99a8adc9523917c65cb8c4b2
kargoolek <kb.appsdesign@gmail.com>
2018-04-29 00:11:29 +0200

    Added subtraction method.

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,7 +1,13 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+
     void sum(double a, double b){
-        System.out.println("result of sum: "+(a+b));
+        System.out.println("result of addition: "+(a+b));
     }
+
+    void subtract(double a, double b){
+        System.out.println("result of subtraction: "+(a-b));
+    }
+
 }
--- src/main/java/com/kodilla/kalkulator/KalkulatorApplication.java
+++ src/main/java/com/kodilla/kalkulator/KalkulatorApplication.java
@@ -8,7 +8,9 @@ public class KalkulatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KalkulatorApplication.class, args);
-        Calculator cal = new Calculator();
-        cal.sum(12.3, 2123.22);
+        Calculator calc = new Calculator();
+        calc.sum(4222.3, 2123.22);
+        calc.subtract(4222.3, 2123.22);
     }
+
 }
